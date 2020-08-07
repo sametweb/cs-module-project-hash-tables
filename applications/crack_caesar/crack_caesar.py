42070950ede38799452759afd418b089f6a73485
sametweb <sametmutevellioglu@gmail.com>
2020-08-07 11:50:21 -0700

    ceaser not finished

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -14,36 +14,25 @@ def crack():
         text = f.read()
 
     count_store = {}
-    lump_text = text.replace(' ', '')
 
-    for letter in lump_text:
+    for letter in text:
         if re.match('[A-Z]', letter):
             if letter in count_store:
                 count_store[letter] += 1
             else:
                 count_store[letter] = 1
 
-    total_letters = 0
-    for item in count_store.items():
-        total_letters += item[1]
+    count_store_items = list(count_store.items())
+    count_store_items.sort(key=lambda x: x[1], reverse=True)
+    count_store_items = [item[0] for item in count_store_items]
 
-    freq_store = {}
+    for i in range(len(count_store_items)):
+        text = text.replace(count_store_items[i], frequents[i])
 
-    for key, value in count_store.items():
-        freq_store[key] = round(value / total_letters * 100, 2)
-
-    freq_store_items = list(freq_store.items())
-    freq_store_items.sort(key=lambda x: x[1], reverse=True)
-    freq_store_items = [item[0] for item in freq_store_items]
-
-    result_text = text
-    for i in range(len(freq_store_items)):
-        result_text = result_text.replace(freq_store_items[i], frequents[i])
-
-    print(freq_store)
-    print(freq_store_items)
+    print(count_store)
+    print(count_store_items)
     print(frequents)
-    # return result_text
+    # return text
 
 
 print(crack())
